fb0b27144ee1d1edf40b779abcc2c41e54a494f9
vuipham <vui25318@gmail.com>
2023-02-11 22:25:57 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const cors = require('cors')
-
 const app = express()
+
+const contactsRouter = require("./app/routes/contact.route");
 
 app.use(cors())
 app.use(express.json())
@@ -9,5 +10,7 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.json({ message: "Welcome to contact book application. hehe" })
 })
+
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app
