4bbb042834b891b0b916818b0c6b18dacaa9d1db
vuipham <vui25318@gmail.com>
2023-02-15 14:36:51 +0700

    backend_p1

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const app = express()
 
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 app.use(cors())
 app.use(express.json())
@@ -12,5 +13,19 @@ app.get('/', (req, res) => {
 })
 
 app.use("/api/contacts", contactsRouter);
+app.use((req, res, next) => {
+  // Code ở đây sẽ chạy khi không có route được định nghĩa nào
+  // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+  // res.status(404).send("Sorry can't find that!")
+  return next(new ApiError(404, "Resource not found"));
+});
+app.use((err, req, res, next) => {
+  // Middleware xử lý lỗi tập trung.
+  // Trong các đoạn code xử lý ở các route, gọi next(error)
+  // sẽ chuyển về middleware xử lý lỗi này
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
 
 module.exports = app
